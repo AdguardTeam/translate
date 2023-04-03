abc4609e896e3a73dfd58055c076b83d571bbfbc
Stanislav Atroschenko <s.atroschenko@adguard.com>
2023-04-03 12:42:38 +0300

    Pull request #13: AG-20972 export Locales type and update yarn.lock
    Merge in EXTENSIONS/translate from feature/AG-20972 to master
    
    Squashed commit of the following:
    
    commit 66bc2e64d0b5cdda32b46cefef5e3f518230d9b6
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Thu Mar 30 14:27:57 2023 +0300
    
        increment
    
    commit 2ae7dbd35d67da5ce1492fc44f96a230767da11e
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Thu Mar 30 14:18:35 2023 +0300
    
        export Locales type and update yarn.lock

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { I18nInterface, Translator } from './src/Translator';
 export { translate } from './src/translate';
 export { validator } from './src/validator';
+export { Locales } from './src/plural';
